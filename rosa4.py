d3eecc1fb9301066d4cb12ce4d4183420718d0ef
Gopal Srivastava <71832523+killbill-del@users.noreply.github.com>
2020-10-22 13:00:31 +0530

    Delete rosa4.py

--- rosa4.py
+++
@@ -1,8 +0,0 @@
-
-with open("rosalind_ini4-2.txt", "r") as fp:
-    data = fp.read().split()
-    a = int(data[0])
-    b = int(data[1])
-    odds = [x for x in range(a,b+1) if x%2==1]
-    print(sum(odds))
-        
